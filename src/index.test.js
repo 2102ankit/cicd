953faf011ab55197a2a6b5bfe78b84e543c683e6
2102ankit <105378102+2102ankit@users.noreply.github.com>
2023-11-26 11:11:17 +0530

    Multiply divide added

--- src/index.test.js
+++ src/index.test.js
@@ -1,20 +1,25 @@
 // import files
-var add = require('./calculator/add')
-var subtract = require('./calculator/subtract')
-var multiply = require('./calculator/multiply')
-var divide = require('./calculator/divide')
-
+var add = require("./calculator/add");
+var subtract = require("./calculator/subtract");
+var multiply = require("./calculator/multiply");
+var divide = require("./calculator/divide");
 
 // Test Case 1 (Addition)
 test("Should add two numbers", () => {
-  expect(add(25, 10)).toBe(35);
+  expect(add(20, 10)).toBe(30);
 });
 
 // Test Case 2 (Subtraction)
 test("Should subtract two numbers", () => {
-  expect(subtract(25, 10)).toBe(15);
+  expect(subtract(20, 10)).toBe(10);
 });
 
 // Test Case 3 (Multiplication)
+test("Should multiply two numbers", () => {
+  expect(multiply(20, 10)).toBe(200);
+});
 
 // Test Case 4 (Division)
+test("Should divide two numbers", () => {
+  expect(divide(20, 10)).toBe(2);
+});
